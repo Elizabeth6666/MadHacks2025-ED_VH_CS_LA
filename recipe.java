2a03f82831d635a929e36c6077c2448857ba3be1
Elizabeth6666 <elliead06@gmail.com>
2025-11-22 13:11:48 -0600

    added files

---
+++ recipe.java
@@ -0,0 +1,15 @@
+class Recipe {
+    private String[] ingredients; //Ingredients
+    private String recipeName; // Recipe Name
+    private String[] allergens; //Allergens
+    private double cookTimeInMinutes; // Cook Time in Minutes
+    private String cookMethod;  //Cook Method (No cook, microwave, oven, etc)
+
+    public Recipe(String name) {
+        this.recipeName = name;
+    }
+
+    public void(String ingredient) {
+        
+    }
+}
