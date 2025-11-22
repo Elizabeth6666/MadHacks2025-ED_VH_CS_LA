8aae7ba6190337012edf018371c454274c5f3a61
Elizabeth6666 <elliead06@gmail.com>
2025-11-22 14:42:03 -0600

    Test Commit

---
+++ mysql.sql
@@ -0,0 +1,6 @@
+-- CREATE TABLE RECIPE (recName TEXT NOT NULL PRIMARY KEY, ingredient TEXT NOT NULL);
+
+-- INSERT INTO recipe (recName, ingredient)
+-- VALUES ("TEST", "Test2");
+
+SELECT * FROM RECIPE
