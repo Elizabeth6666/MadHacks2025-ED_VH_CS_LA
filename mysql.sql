437c8cadc96c2e61b453840e6b8193387fa9d84f
Elizabeth6666 <elliead06@gmail.com>
2025-11-22 15:06:32 -0600

    Tables created

--- mysql.sql
+++
@@ -1,6 +0,0 @@
--- CREATE TABLE RECIPE (recName TEXT NOT NULL PRIMARY KEY, ingredient TEXT NOT NULL);
-
--- INSERT INTO recipe (recName, ingredient)
--- VALUES ("TEST", "Test2");
-
--- SELECT * FROM RECIPE
